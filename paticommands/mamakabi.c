408a88b0a55eed9f5b0e36f78ecb9d6931f11f84
Mehmet Demir <mehmetd@vuhuv.com>
2026-05-10 15:27:33 +0300

    Made it more readable

--- paticommands/mamakabi.c
+++ paticommands/mamakabi.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argv, char *argc[]) {
-    char girdi1[256];
-    FILE *ofile = fopen("/proc/meminfo","r");
-    if (ofile == NULL) {
-       printf("MAMAM YOK CALDİLAR D:\n");
-       return 1;
-}
-    while (fgets(girdi1, 256, ofile) != NULL) {
-        printf("%s", girdi1);
-}
-    fclose(ofile);
-    printf("\n");
+int main() {
+    FILE *f = fopen("/proc/meminfo", "r");
+    if (!f) { printf("MAMAM YOK CALDİLAR D:\n"); return 1; }
 
+    char satir[256];
+    unsigned long toplam = 0, kullanilabilir = 0, bos = 0, takas_toplam = 0, takas_bos = 0;
+        while (fgets(satir, sizeof(satir), f)) {
+        unsigned long val;
+        if      (sscanf(satir, "MemTotal: %lu kB", &val) == 1) toplam = val;
+        else if (sscanf(satir, "MemAvailable: %lu kB", &val) == 1) kullanilabilir = val;
+        else if (sscanf(satir, "MemFree: %lu kB", &val) == 1) bos = val;
+        else if (sscanf(satir, "SwapTotal: %lu kB", &val) == 1) takas_toplam = val;
+        else if (sscanf(satir, "SwapFree: %lu kB", &val) == 1) takas_bos = val;
+    }
+    fclose(f);
+
+    unsigned long kullanilan = toplam - kullanilabilir;
+    int yuzde = toplam > 0 ? (kullanilan * 100 / toplam) : 0;
+
+    printf("MAMA KABI -- Bellek Durumu\n");
+    printf("---------------------------------\n");
+    printf("Toplam  : %5lu MB\n", toplam / 1024);
+    printf("Kullanim: %5lu MB (%d%%)\n", kullanilan / 1024, yuzde);
+    printf("Bos     : %5lu MB\n", kullanilabilir / 1024);
+    if (takas_toplam > 0)
+        printf("Takas   : %5lu MB\n", takas_toplam / 1024);
+
+    return 0;
 }
+

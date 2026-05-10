13f162abbe68981087ec89715626b3fbb08e303a
Mehmet Demir <mehmetd@vuhuv.com>
2026-05-10 15:26:59 +0300

    Added -a (all) prefix to show the all active processes

--- paticommands/karabas.c
+++ paticommands/karabas.c
@@ -29,12 +29,14 @@ void karabas() {
         char dosyayolu[256] = {0};
         while (fgets(dosya, sizeof(dosya), pcgfile) != NULL) {
             char kopya[256];
-            strcpy(kopya, dosya);
+            strncpy(kopya, dosya, sizeof(kopya) - 1);
+            kopya[sizeof(kopya)-1] = '\0';
             char *okuyucu = strtok(kopya, " =\n");
             if (okuyucu == NULL) continue;
             if (strcmp(okuyucu, "konumu") == 0) {
                 char *gecici = strtok(NULL, " =\n");
-                if (gecici != NULL) strcpy(dosyayolu, gecici);
+                if (gecici != NULL) strncpy(dosyayolu, gecici, sizeof(dosyayolu) - 1);
+                dosyayolu[sizeof(dosyayolu)-1] = '\0';
             }
         }
         fclose(pcgfile);
@@ -56,7 +58,8 @@ void karabas() {
                     fclose(cmdfile);
                     if (strcmp(cmdline, dosyayolu) == 0) {
                         char adkopya[256];
-                        strcpy(adkopya, dosyayolu);
+                        strncpy(adkopya, dosyayolu, sizeof(adkopya) - 1);
+                        adkopya[sizeof(adkopya)-1] = '\0';
                         printf("%-20s PID: %s\n", basename(adkopya), proclist[j]->d_name);
                         bulundu = 1;
                     }
@@ -68,15 +71,47 @@ void karabas() {
 
         if (bulundu == 0) {
             char adkopya[256];
-            strcpy(adkopya, dosyayolu);
+            strncpy(adkopya, dosyayolu, sizeof(adkopya) - 1);
+            adkopya[sizeof(adkopya)-1] = '\0';
             printf("%-20s [calismiyor]\n", basename(adkopya));
         }
         free(namelist[i]);
     }
     free(namelist);
 }
+void ps_tumu() {
+    struct dirent **proclist;
+    int np = scandir("/proc", &proclist, NULL, alphasort);
+    if (np < 0) { printf("proc okunamadi\n"); return; }
 
-int main(void) {
-    karabas();
+    printf("%-20s %s\n", "Komut", "PID");
+    printf("----------------------------------\n");
+    for (int j = 0; j < np; j++) {
+        if (atoi(proclist[j]->d_name) > 0) {
+            char yol[512];
+            snprintf(yol, sizeof(yol), "/proc/%s/cmdline", proclist[j]->d_name);
+            FILE *f = fopen(yol, "r");
+            if (f) {
+                char cmd[256] = {0};
+                fread(cmd, 1, sizeof(cmd) - 1, f);
+                fclose(f);
+                if (cmd[0]) {
+                    char *ad = strrchr(cmd, '/');
+                    if (ad) ad++; else ad = cmd;
+                    printf("%-20s PID: %s\n", ad, proclist[j]->d_name);
+                }
+            }
+        }
+        free(proclist[j]);
+    }
+    free(proclist);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1 && strcmp(argv[1], "-a") == 0) {
+        ps_tumu();
+    } else {
+        karabas();
+    }
     return 0;
 }

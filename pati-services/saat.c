01399aa4164852afba84f947ba82d146f36ba6b4
Mehmet Demir <mehmetd@vuhuv.com>
2026-05-10 15:29:19 +0300

    Closed TOCTOU (Time of check/use) vulnerability

--- pati-services/saat.c
+++ pati-services/saat.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/rtc.h>
+#include <time.h>
 
 int main() {
     printf("Saat servisi baslatiliyor...\n");
     fflush(stdout);
 
-    int fd = open("/dev/rtc0", O_RDONLY);
-    if (fd < 0) {
-        perror("RTC acilamadi");
-        return -1;
-    }
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
 
-    struct rtc_time rtc_tm;
-    int ret = ioctl(fd, RTC_RD_TIME, &rtc_tm);
-    if (ret < 0) {
-        perror("RTC okunamadi");
-        close(fd);
+    if (t == NULL) {
+        perror("zaman alinamadi");
         return -1;
     }
 
     printf("Bismillahirahmanirrahim: %d-%d-%d %02d:%02d:%02d\n",
-        rtc_tm.tm_mday, rtc_tm.tm_mon + 1, rtc_tm.tm_year + 1900,
-        rtc_tm.tm_hour + 3, rtc_tm.tm_min, rtc_tm.tm_sec);
+        t->tm_mday, t->tm_mon + 1, t->tm_year + 1900,
+        t->tm_hour + 3, t->tm_min, t->tm_sec);
     fflush(stdout);
 
-    close(fd);
     return 0;
 }
